e4f7d6835220b41530f9a5a2ca95529825321800
Glen Mailer <glen.mailer@bskyb.com>
2011-05-09 21:05:22 +0100

    refactored tidier hole6, and aliased self

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,17 +1,18 @@
 class Golf
-  def self.hole1 a
+  s = self
+  def s.hole1 a
     a.inject :*
   end
   
-  def self.hole2 s
+  def s.hole2 s
     s.split.sort_by { |w| w[1] } * ' '
   end
   
-  def self.hole3 n
+  def s.hole3 n
     n == 1 ? 1 : n * hole3(n-1)
   end
   
-  def self.hole4 a
+  def s.hole4 a
     [
       [/man\(.*\)/, "hat(\\0)"],
       [/dog\(\w+/, "\\0(bone)"],
@@ -22,28 +23,34 @@ class Golf
     a
   end
   
-  def self.hole5 a
+  def s.hole5 a
     (1..4).map {|n| (0..4-n).map { |i| a[i, n] }}.inject(:+)
   end
   
-  def self.hole6 a
-    (1..a).map { |i| i % 15 == 0 ? 'fizzbuzz' : i % 3 == 0 ? 'fizz' : i % 5 == 0 ? 'buzz' : i }
+  def s.hole6 a
+    (1..a).map { |i|
+      n = ''
+      n << 'fizz' if i % 3 == 0
+      n << 'buzz' if i % 5 == 0
+      n != '' && n || i
+    }
   end
   
-  def self.hole8 n
+  def s.hole8 n
     f = proc { |i| i < 3 ? 1 : f[i-1] + f[i-2] }
     (1..n).map(&f)
   end
   
-  def self.hole9 f
+  def s.hole9 f
     v = File.read(f).split(/\n/).map { |l| l.split ", " }
     while true
       c = Hash.new(0)
       v.map(&:first).each { |i| i && c[i] += 1 }
       r = c.invert
-      a = r.keys.max
-      return r[a] if a > (v.size/2.0) || c.size == 2
-      v.map { |p| p.reject! { |s| s == r[r.keys.min] } }
+      k = r.keys
+      a = k.max
+      return r[a] if a > v.size/2.0 || c.size == 2
+      v.map { |p| p.reject! { |s| s == r[k.min] } }
     end
   end
 end
