beeeea049c28cefc0972078399fa08f3ddb26b1b
Ian White <ian.w.white@gmail.com>
2011-05-09 20:13:04 +0100

    First crack

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,60 @@
 class Golf
+  class << self
+    def hole1(a)
+      a.inject(1){|m,i| i*m}
+    end
+    
+    def hole2(s)
+      s.split(' ').sort_by{|w| w[1]}.join(' ')
+    end
+    
+    def hole3(n)
+      hole1(1..n)
+    end
+    
+    def hole4(a)
+      a.map {|i| i.sub(/man(.*)/, 'hat(\0)') }.
+        map {|i| i.sub(/(dog.*)\)/,'\1(bone))') }.
+        map {|i| i.sub('cat','dead')}
+    end
+    
+    def hole5(a)
+      (0..a.size).inject([]){|m,i| m + (0..a.size-i-1).map {|j| a[j..j+i] }  }
+    end
+    
+    def hole6(n)
+      (1..n).map{|i| (i%3 == 0 && i%5 == 0 && 'fizzbuzz') || (i%3 == 0  && 'fizz') || (i%5 == 0 && 'buzz') ||  i }
+    end
+    
+    def hole8(n)
+      a = [1,1]
+      (2..n-1).each {|i| a << a[-2..-1].inject(0){|m,j| m+j}; }
+      a
+    end
+    
+    def hole9(f)
+      v = File.read(f).split("\n").map{|l| l.split(', ')}
+      count(v)
+    end
+    
+    def count(v)
+      
+      c = v.flatten.uniq.inject({}){|m,i| m.merge(i => 0)}
+      
+      v.each {|can| c[can[0]] += 1}
+      
+      sorted = c.to_a.sort_by{|i| i[1]}
+      least = sorted[0]
+      most = sorted[-1]
+      
+      return most[0] if most[1] > v.size/2.0
+      v = reject_and_dist(least[0], v)
+      
+      count(v)
+    end
+    
+    def reject_and_dist(can, votes)
+      votes.map {|v| v - [can] }.reject(&:empty?)
+    end
+  end
 end
--- spec/golf_spec.rb
+++ spec/golf_spec.rb
@@ -96,30 +96,30 @@ describe "Golf" do
     it "should give the first N numbers of the fibonacci sequence" do
       Golf.hole8(5).should eql [1,1,2,3,5]
     end
-
+  
     it "should give the first 9 numbers" do
       Golf.hole8(9).should eql [1,1,2,3,5,8,13,21,34]
     end
-
+  
     it "should give the first 15 numbers" do
       Golf.hole8(15).should eql [1,1,2,3,5,8,13,21,34,55,89,144,233,377,610]
     end
   end
-
-
+  
+  
   describe ".hole9" do
     it "should return the winner of an AV election where winner has a first round majority" do
       Golf.hole9('fixtures/av-example1.txt').should eql "blue"
     end
-
+  
     it "should eliminate the last candidate if there is no first round majority and allocate their second choices" do
       Golf.hole9('fixtures/av-example2.txt').should eql "red"
     end
-
+  
     it "should continue to eliminate the losing candidate until a majority is reached" do
       Golf.hole9('fixtures/av-example3.txt').should eql "blue"
     end
-
+#  
     it "should be the candidate with most votes if there are only two left, even witout a majority" do
       Golf.hole9('fixtures/av-example4.txt').should eql "blue"
     end
